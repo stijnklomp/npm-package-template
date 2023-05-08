af4d135ded32c7d35fd2b577a7341929bb048536
stijnklomp <stijn@LAPTOP-RB5OL0IG>
2023-05-08 19:44:07 +0100

    Integrating every configuration from '@stijnklomp/eslint-config'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,16 @@
 module.exports = {
 	root: true,
-	extends: [
-		"@stijnklomp/eslint-config/eslint",
-		"eslint:recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended"
-	],
+	extends: ["@stijnklomp/eslint-config/eslint"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint", "prettier"],
+	parserOptions: {
+		ecmaVersion: 2021,
+		srouceType: "module"
+	},
 	env: {
 		browser: true,
 		node: true,
 		commonjs: true,
 		es6: true
-	},
-	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint", "prettier"],
-	parserOptions: {
-		ecmaVersion: 2021,
-		sourceType: "module"
-	},
-	overrides: [
-		{
-			files: ["**/*.{ts,tsx}"],
-			extends: [
-				"eslint:recommended",
-				"plugin:@typescript-eslint/recommended",
-				"plugin:@typescript-eslint/recommended-requiring-type-checking"
-			],
-			parserOptions: {
-				project: "./tsconfig.json"
-			}
-		}
-	]
+	}
 }
